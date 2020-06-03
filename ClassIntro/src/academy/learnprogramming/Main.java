0e27a8263f77b3d0cff440471d5bc56f38575f3d
japerry911 <jack.e.perry@outlook.com>
2020-06-03 13:55:31 -0600

    ClassIntro completed

---
+++ ClassIntro/src/academy/learnprogramming/Main.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.setModel("Carerra");
+
+        System.out.println(porsche.getModel());
+    }
+}
