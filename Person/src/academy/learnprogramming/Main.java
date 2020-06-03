7bedf7f88dc5eb8f508ce99c7d2b13d74e0eefb6
japerry911 <jack.e.perry@outlook.com>
2020-06-03 14:09:00 -0600

    Person completed

---
+++ Person/src/academy/learnprogramming/Main.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Person person = new Person();
+
+	    person.setFirstName("");
+	    person.setLastName("");
+	    person.setAge(10);
+
+        System.out.println("FullName = " + person.getFullName());
+        System.out.println("teen = " + person.isTeen());
+
+        person.setFirstName("John");
+        person.setAge(18);
+
+        System.out.println("FullName = " + person.getFullName());
+        System.out.println("teen = " + person.isTeen());
+
+        person.setLastName("Smith");
+
+        System.out.println("FullName = " + person.getFullName());
+    }
+}
